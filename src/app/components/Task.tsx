5d669334ac9847d959485353445eedaecb675b1a
DekiWill <William_Watanabe@hotmail.com>
2024-09-21 13:24:35 -0300

    update: ajustes no componente de tas

--- src/app/components/Task.tsx
+++ src/app/components/Task.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./task.module.scss";
 import { DELETE, UPDATE } from "../actions/actions";
 
--- src/app/components/Todos.tsx
+++ src/app/components/Todos.tsx
@@ -5,32 +5,31 @@ import { todoList, todoListCompleted } from "../actions/actions";
 export default async function Todos() {
   const completedTodos = await todoListCompleted();
   const todos = await todoList();
+
   return (
-    <>
-      <main className={styles.todos}>
-        <ul className={styles.todos__list}>
-          <li className={styles.todos__list__title}>Suas Tarefas de hoje</li>
-          {todos.map((todo) => (
-            <Task
-              completed={todo.completed}
-              task={todo.task}
-              id={todo.id}
-              key={todo.id}
-            />
-          ))}
-        </ul>
-        <ul className={styles.todos__list}>
-          <li className={styles.todos__list__title}>Tarefas finalizadas</li>
-          {completedTodos.map((todo) => (
-            <Task
-              completed={todo.completed}
-              task={todo.task}
-              id={todo.id}
-              key={todo.id}
-            />
-          ))}
-        </ul>
-      </main>
-    </>
+    <main className={styles.todos}>
+      <ul className={styles.todos__list}>
+        <li className={styles.todos__list__title}>Suas Tarefas de hoje</li>
+        {todos.map((todo) => (
+          <Task
+            completed={todo.completed}
+            task={todo.task}
+            id={todo.id}
+            key={todo.id}
+          />
+        ))}
+      </ul>
+      <ul className={styles.todos__list}>
+        <li className={styles.todos__list__title}>Tarefas finalizadas</li>
+        {completedTodos.map((todo) => (
+          <Task
+            completed={todo.completed}
+            task={todo.task}
+            id={todo.id}
+            key={todo.id}
+          />
+        ))}
+      </ul>
+    </main>
   );
 }
